e5691954aac59c6272e2833b52051b046cae5405
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-19 02:07:40 +0000

    Implementing a function that can compare two arrays for a perfect match

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${(actual)} === ${(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${(actual)} !== ${(expected)}`);
+  }
+};
+
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+//Test Cases:
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
