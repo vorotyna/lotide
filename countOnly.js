59673c7d8f18686ee33332cd0b0103c952dc3326
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-19 16:19:51 +0000

    Count them all

--- countOnly.js
+++ countOnly.js
@@ -12,7 +12,11 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    console.log(item);
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
   }
   return results;
 };
