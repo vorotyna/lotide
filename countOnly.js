a78bf5df0f10d502157ba1a64b8c7148ac1605e1
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-19 16:27:16 +0000

    Only count specific items

--- countOnly.js
+++ countOnly.js
@@ -10,12 +10,13 @@ const assertEqual = function(actual, expected) {
 // ACTUAL FUNCTION
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-
   for (const item of allItems) {
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
   }
   return results;
