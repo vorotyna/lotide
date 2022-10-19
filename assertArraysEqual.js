fcdea47cdb9972c09a077ae80cc050c00dacec8b
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-19 03:22:46 +0000

    Asserting arrays that are equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (actual === true) {
+    console.log(`✅✅✅ Assertion Passed: ${(actual)} === ${(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${(actual)} !== ${(expected)}`);
+  }
+};
+
+//Test Case:
+assertArraysEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), true); // => should FAIL
