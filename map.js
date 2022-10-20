eb0cc0a0be7dad0690ea158f9caf2c012af03eac
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-20 22:56:58 +0000

    Creating a .map function

---
+++ map.js
@@ -0,0 +1,39 @@
+// ASSERT
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${(actual)} === ${(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${(actual)} !== ${(expected)}`);
+  }
+};
+
+// ACTUAL FUNCTION
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+
+const results2 = map(words, word => word.length);
+assertArraysEqual(results2, [6, 7, 2, 5, 3]);
+
+const results3 = map(words, word => word[5]);
+assertArraysEqual(results3, ['d', 'o', undefined, undefined, undefined]);
