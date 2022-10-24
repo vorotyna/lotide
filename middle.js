a185abb46c2fe9244c6a80a8dbba93e1e2020ee7
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-24 20:45:47 +0000

    Modularizing middle

--- middle.js
+++ middle.js
@@ -1,26 +1,6 @@
-// TEST/ASSERTION FUNCTIONS
-const eqArrays = function(arrOne, arrTwo) {
-  if (arrOne.length !== arrTwo.length) {
-    return false;
-  }
-  for (i = 0; i < arrOne.length; i++) {
-    if (arrOne[i] !== arrTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
-const assertArraysEqual = function(actual, expected) {
-  if (actual === true) {
-    console.log(`✅✅✅ Assertion Passed: ${(actual)} === ${(expected)}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${(actual)} !== ${(expected)}`);
-  }
-};
-
-
-// ACTUAL FUNCTION
 const middle = function(arr) {
   let revisedArr = [];
   let index = Math.round((arr.length) / 2);
@@ -35,7 +15,4 @@ const middle = function(arr) {
   }
 };
 
-// TEST CODE
-assertArraysEqual(eqArrays(middle([1, 2, 3]), [2]), true); // -> Should pass
-assertArraysEqual(eqArrays(middle([1, 2, 3, 6]), [2]), true); // -> Should fail
-assertArraysEqual(eqArrays(middle([1]), []), true); // -> Should pass
+module.exports = middle;
