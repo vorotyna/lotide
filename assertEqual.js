a53d234bff9a669e352c5bb7b9b8612649deb5b0
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-24 20:06:52 +0000

    Modularizing assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,3 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${(actual)} === ${(expected)}`);
@@ -7,6 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
