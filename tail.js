8c90fe62ec3af6774fd2a34e28f71e4a99d1962f
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-24 20:16:11 +0000

    Modularizing tail

--- tail.js
+++ tail.js
@@ -1,11 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${(actual)} === ${(expected)}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${(actual)} !== ${(expected)}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 function tail(actual, expected) {
   return actual.slice(1);
 };
+
+module.exports = tail;
