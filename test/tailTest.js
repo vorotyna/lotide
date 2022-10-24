f40c36036bfa781f2da2fc0dfa446d5ea1b3d836
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-24 21:24:26 +0000

    Incorporate Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,14 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ["Hey there", "Lighthouse", "Labs"];
-assertEqual(words.length, 3);
-assertEqual(tail(words).length, 2);
+describe("#tail", () => {
+  it("returns 3 for [Hey there, Lighthouse, Labs]", () => {
+    const words = ["Hey there", "Lighthouse", "Labs"];
+    assert.deepEqual(words.length, 3);
+  });
+  it("returns 2 for tail([Hey there, Lighthouse, Labs])", () => {
+    ;
+    const words = ["Hey there", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(words).length, 2);
+  });
+});
