9c8610acb8823396fe75b90a0c6838d3bf7a77d8
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-24 22:02:30 +0000

    Incorporate Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,17 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(eqArrays(middle([1, 2, 3]), [2]), true); // -> Should pass
-assertArraysEqual(eqArrays(middle([1, 2, 3, 6]), [2]), true); // -> Should fail
-assertArraysEqual(eqArrays(middle([1]), []), true); // -> Should pass
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3] & [2]", () => {
+    const arrOne = [1, 2, 3];
+    const arrTwo = [2];
+    const expected = [2];
+    assert.deepEqual(middle(arrOne, arrTwo), expected);
+  });
+  it("returns true for [] is middle of [1] (when there is one number in array)", () => {
+    const arrOne = [1];
+    const arrTwo = [];
+    const expected = [];
+    assert.deepEqual(middle(arrOne, arrTwo), expected);
+  });
+});
